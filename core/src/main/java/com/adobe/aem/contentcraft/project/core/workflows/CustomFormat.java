a129291eb5b3dac0bb2c5d4ebbefeb1b6e01607a
Deepa-AILU <72554392+Deepa-AILU@users.noreply.github.com>
2024-02-26 18:59:00 +0530

    Update CustomFormat.java

--- core/src/main/java/com/adobe/aem/contentcraft/project/core/workflows/CustomFormat.java
+++ core/src/main/java/com/adobe/aem/contentcraft/project/core/workflows/CustomFormat.java
@@ -49,7 +49,9 @@ public class ExcelToJsonConverter {
             String jsonString = objectMapper.writeValueAsString(jsonDataList);
 
             System.out.println(jsonString);
-
+private static Boolean validateHeaderByKey(String header) {
+    return Set.of(LIEN_POSITION, AMOUNT_LOW, AMOUNT_HIGH, SCORE_LOW, CLTV_LOW, STATE, PRICE).contains(header);
+        }
             workbook.close();
             inputStream.close();
 
